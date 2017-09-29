ce2ffba37f51fb15fbf3b2121ba0670373af44e3
gasolinewaltz <gasolinewaltz@gmail.com>
2017-09-29 14:06:16 -0700

    took some of the burden away from the itemList component. I think it provides a better user experience to have two adjacent dropdowns for dividing sections and subsections

--- src/components/Pages.vue
+++ src/components/Pages.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="pages-editable">
-    <template v-for="page in pages">
-      <router-link :to="{path: '/dashboard/'+page.title  }">{{page.title}}</router-link>
-    </template>
-    <div class="sub-pages-editable">
-      <template v-for="subpage in subPages">
-        <router-link :to="{path: '/dashboard/'+page+'/'+subpage.title  }">{{subpage.title}}</router-link>
-      </template>
+    <h1>Site Pages</h1>
+    <div class="page-link-container" v-for="_page in pages">
+      <router-link class="page-link" :class="{selected : page===_page.title}" :to="{path: '/dashboard/'+_page.title  }">
+          <i class="fa fa-pencil" aria-hidden="true"></i> {{_page.title}}
+      </router-link>
     </div>
-  </div>
+    <div class="dropdown-group">
+      <div v-show="page" class="sub-pages-editable subpage-select-group">
+        <select @change="subPageSelect">
+          <option class="default" disabled selected=true>-Select Section - </option>
+          <template v-for="subpage in subPages">
+            <option :data-id="subpage.title" :value="'/dashboard/'+page+'/'+subpage.title">
+              {{subpage.title}}
+            </option>
+          </template>
+        </select>
+      </div>
+      <div v-show="siteSection" class="section-select-group" >
+        <select @change="headerSelect">
+          <option class="default" disabled selected="1">- Select Sub Section -</option>
+          <template v-for="header in itemListHeaders">
+            <option :data-id="header" :value="link+header">
+              {{header}}
+            </option>
+          </template>
+        </select>
+      </div>
+    </div>
+    </div>
 </template>
 
 <script type="text/babel">
@@ -23,6 +43,18 @@
       computed:{
         page(){
           return this.$route.params.page;
+        },
+        itemListHeaders(){
+          return this.$store.getters.getItemListHeaders;
+        },
+        siteSection(){
+          return this.$route.params.subpage;
+        },
+        link(){
+          return "/dashboard/"+this.urlFragment;
+        },
+        urlFragment(){
+          return this.$route.params.page+'/'+this.$route.params.subpage+'/';
         }
       },
       methods:{
@@ -38,6 +70,7 @@
             });
 
             this.$data.pages = response.data;
+
           });
         },
         getSubpages(){
@@ -62,8 +95,26 @@
         getContent(){
           this.getPages();
           this.getSubpages();
-
           this.$store.commit('setCurrentPage', this.$route.params.page);
+        },
+        subPageSelect(evt){
+          this.$el.querySelector('.section-select-group select option').selected = true;
+          this.$router.replace(evt.target.value);
+        },
+        headerSelect(evt){
+          this.$router.replace(evt.target.value)
+        },
+        setDropDowns(){
+          if(this.$route.params.subpage){
+           Array.prototype.slice.call(this.$el.querySelectorAll('.subpage-select-group select option'), 0).filter(option =>{
+              return option.dataset.id === this.$route.params.subpage;
+            })[0].selected = true;
+          }
+          if(this.$route.params.section){
+            Array.prototype.slice.call(this.$el.querySelectorAll('.section-select-group select option'), 0).filter(option =>{
+              return option.dataset.id === this.$route.params.section;
+            })[0].selected = true;
+          }
         }
       },
       watch:{
@@ -73,7 +124,11 @@
       },
       created(){
         this.getContent();
-
+        EventBus.$on('itemListLoad', () =>{
+          this.$nextTick(()=>{
+            this.setDropDowns();
+          });
+        });
       }
     }
 </script>
